6e345b3b8c2ae3e2419746f83fc6c87a9c30216c
valdas <pliksny3@gmail.com>
2017-12-14 23:55:52 +0200

    7 temos nd.

--- tema7.php
+++ tema7.php
@@ -7,10 +7,37 @@
 <body>
 
 <?php
-function fak($fname,$lname){
-    return "hi $fname $lname!";
+//Masyvu vidurkiu skirtumas
+/*
+$a=array(5,6,10,15);
+$b=array(8,5,3,25);
+//Suranda masyvo vidurki
+function vidurkis(array $array){
+    $sum=0;
+    for($x=0;$x<count($array);$x++){
+        $sum+=$array[$x];
+    }
+    $vid=$sum/count($array);
+    return $vid;
 }
-echo fak("valdas","ponas");
+var_dump(vidurkis($a)-vidurkis($b));
+*/
+
+//Tobuli skaiciai be funkciju
+/*
+for($x=1;$x<=1000;$x++){
+    $sum=0;
+    for($y=1;$y<=($x/2);$y++){
+        if($x%$y==0){
+            $sum+=$y;
+        }
+    }
+    //echo $x.'+'.$sum.'<br>';
+    if($sum==$x){
+        echo $x.'<br>';
+    }
+}
+*/
 ?>
 </body>
 </html>
